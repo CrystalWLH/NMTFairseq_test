a1de37f623e84feef6deeb97ce145faa0239460b
CrystalWLH <18811717717@163.com>
2020-03-25 20:49:36 +0800

    run script

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 #deal data
 #python preprocess.py --source-lang ti --seg-lang seg --target-lang zh --trainpref ../../../data/nmt_seg/train --validpref ../../../data/nmt_seg/valid --testpref ../../../data/nmt_seg/test-2017 --destdir mydata/data-bin/nmt_seg_bin --workers 5
+#python preprocess.py --source-lang seg --seg-lang seg --target-lang zh --trainpref ../../../data/nmt_seg_TiZh/train --validpref ../../../data/nmt_seg_TiZh/valid --testpref ../../../data/nmt_seg_TiZh/test-2017 --destdir mydata/data-bin/nmt_seg_bin_wordWord --workers 5 --nwordssrc 50000 --nwordsseg 50000 --nwordstgt 50000
 
 #python preprocess.py --source-lang zh --seg-lang seg --target-lang en --trainpref ../../../data/nmt_seg_ZhEn/train --validpref ../../../data/nmt_seg_ZhEn/valid --testpref ../../../data/nmt_seg_ZhEn/test --destdir mydata/data-bin/nmt_seg_ZhEn_bin --workers 5 --nwordssrc 6000 --nwordsseg 50000 --nwordstgt 50000
+#python preprocess.py --source-lang seg --seg-lang seg --target-lang en --trainpref ../../../data/nmt_seg_ZhEn/train --validpref ../../../data/nmt_seg_ZhEn/valid --testpref ../../../data/nmt_seg_ZhEn/test --destdir mydata/data-bin/nmt_seg_ZhEn_bin_wordWord --workers 5 --nwordssrc 50000 --nwordsseg 50000 --nwordstgt 50000
 
 #train transformer model
 #CUDA_VISIBLE_DEVICES=0 python train.py mydata/data-bin/nmt_seg_bin --source-lang ti --target-lang zh --seg-lang seg --task seg_translation --arch seg_nmt_ctc_lstm --save-dir checkpoints/exp1 --log-interval 300 --no-progress-bar --no-epoch-checkpoints --log-format simple --share-decoder-input-output-embed --optimizer adam --adam-betas '(0.9,0.98)' --clip-norm 0.0 --lr 5e-4  --criterion ctc_nmt_loss --max-tokens 4096
@@ -17,7 +19,11 @@
 #---------------bidirection------------
 #char-word的baseline
 #CUDA_VISIBLE_DEVICES=1 python train.py mydata/data-bin/nmt_seg_bin --source-lang ti --target-lang zh --seg-lang seg --task seg_translation --arch my_ctc_nmt_lstm --save-dir checkpoints/exp4 --log-interval 300 --no-progress-bar --no-epoch-checkpoints --log-format simple --optimizer adam --lr 0.0005 --lr-shrink 0.5 --criterion ctc_nmt_loss --max-tokens 4096 --ctc-weight 0 --nmt-weight 1 --dropout 0.3 --nmt-encoder-hidden-size 1024
-CUDA_VISIBLE_DEVICES=0 python train.py mydata/data-bin/nmt_seg_ZhEn_bin --source-lang zh --target-lang en --seg-lang seg --task seg_translation --arch my_ctc_nmt_lstm --save-dir checkpoints/exp4_zhen --log-interval 1000 --no-progress-bar --no-epoch-checkpoints --log-format simple --optimizer adam --lr 0.0005 --lr-shrink 0.5 --criterion ctc_nmt_loss --max-tokens 2048 --ctc-weight 0 --nmt-weight 1 --dropout 0.3 --nmt-encoder-hidden-size 1024
+#CUDA_VISIBLE_DEVICES=0 python train.py mydata/data-bin/nmt_seg_ZhEn_bin --source-lang zh --target-lang en --seg-lang seg --task seg_translation --arch my_ctc_nmt_lstm --save-dir checkpoints/exp4_zhen --log-interval 1000 --no-progress-bar --no-epoch-checkpoints --log-format simple --optimizer adam --lr 0.0005 --lr-shrink 0.5 --criterion ctc_nmt_loss --max-tokens 2048 --ctc-weight 0 --nmt-weight 1 --dropout 0.3 --nmt-encoder-hidden-size 1024
+
+#word-word的baseline
+#CUDA_VISIBLE_DEVICES=1 python train.py mydata/data-bin/nmt_seg_bin_wordWord --source-lang seg --target-lang zh --seg-lang seg --task seg_translation --arch my_ctc_nmt_lstm --save-dir checkpoints/exp4_wordWord --log-interval 300 --no-progress-bar  --log-format simple --optimizer adam --lr 0.0005 --lr-shrink 0.5 --criterion ctc_nmt_loss --max-tokens 4096 --ctc-weight 0 --nmt-weight 1 --dropout 0.3 --nmt-encoder-hidden-size 1024 --save-interval 20
+#CUDA_VISIBLE_DEVICES=1 python train.py mydata/data-bin/nmt_seg_ZhEn_bin_wordWord --source-lang seg --target-lang en --seg-lang seg --task seg_translation --arch my_ctc_nmt_lstm --save-dir checkpoints/exp4_zhen_wordWord --log-interval 1000 --no-progress-bar --log-format simple --optimizer adam --lr 0.0005 --lr-shrink 0.5 --criterion ctc_nmt_loss --max-tokens 2048 --ctc-weight 0 --nmt-weight 1 --dropout 0.3 --nmt-encoder-hidden-size 1024
 
 #char-word的ctc训练
 #CUDA_VISIBLE_DEVICES=0 python train.py mydata/data-bin/nmt_seg_bin --source-lang ti --target-lang zh --seg-lang seg --task seg_translation --arch my_ctc_nmt_lstm --save-dir checkpoints/exp5 --log-interval 300 --no-progress-bar --no-epoch-checkpoints --log-format simple --optimizer adam --lr 0.0005 --lr-shrink 0.5 --criterion ctc_nmt_loss --max-tokens 4096 --ctc-weight 0 --nmt-weight 1 --dropout 0.3 --nmt-encoder-hidden-size 1024
